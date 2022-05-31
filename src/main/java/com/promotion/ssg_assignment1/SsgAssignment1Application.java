693ef68e69c44b77a681613caf16c3c02bf5fdec
juneyoungdo <ehwnsdud1004@naver.com>
2022-05-31 20:44:21 +0900

    [INIT]

---
+++ src/main/java/com/promotion/ssg_assignment1/SsgAssignment1Application.java
@@ -0,0 +1,13 @@
+package com.promotion.ssg_assignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsgAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsgAssignment1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/promotion/ssg_assignment1/SsgAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.promotion.ssg_assignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsgAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
