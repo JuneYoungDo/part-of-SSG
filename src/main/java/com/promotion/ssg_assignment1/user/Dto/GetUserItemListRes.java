da11814c489b1cf3fa8e33af177d0d1edc640137
juneyoungdo <ehwnsdud1004@naver.com>
2022-06-09 02:15:26 +0900

    [SSG_17] 사용자가 구매할 수 있는 상품 정보 반환 기능

---
+++ src/main/java/com/promotion/ssg_assignment1/user/Dto/GetUserItemListRes.java
@@ -0,0 +1,21 @@
+package com.promotion.ssg_assignment1.user.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class GetUserItemListRes {
+    private Long itemId;
+    private String name;
+    private String type;
+    private double price;
+    private LocalDate displayStartDate;
+    private LocalDate displayEndDate;
+}
