207eeb2c7084468c4ae1aeb9df25ff161ae96acb
juneyoungdo <ehwnsdud1004@naver.com>
2022-06-09 16:35:41 +0900

    [SSG_19] 상품 입력 및 프로모션 입력 테스트 추가

---
+++ src/test/java/com/promotion/ssg_assignment1/item/ItemControllerTest.java
@@ -0,0 +1,57 @@
+package com.promotion.ssg_assignment1.item;
+
+import com.promotion.ssg_assignment1.item.Dto.CreateItemReq;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+@WebAppConfiguration
+public class ItemControllerTest {
+
+    private static ValidatorFactory validatorFactory;
+    private static Validator validatorInjected;
+
+    @BeforeAll
+    public static void init() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validatorInjected = validatorFactory.getValidator();
+    }
+
+    @AfterAll
+    public static void close() {
+        validatorFactory.close();
+    }
+
+    @Test
+    @DisplayName("[상품 입력 테스트]")
+    void 상품_입력_테스트() {
+        CreateItemReq createItemReq = new CreateItemReq(
+                "", "TEST_TYPE", "",
+                LocalDate.of(2022, 01, 01),
+                LocalDate.of(2022, 01, 02)
+        );
+        Set<ConstraintViolation<CreateItemReq>> validate = validatorInjected.validate(createItemReq);
+
+        Iterator<ConstraintViolation<CreateItemReq>> iterator = validate.iterator();
+        List<String> messages = new ArrayList<>();
+        while (iterator.hasNext()) {
+            ConstraintViolation<CreateItemReq> next = iterator.next();
+            messages.add(next.getMessage());
+        }
+        Assertions.assertThat(messages).contains("상품 이름은 공백일 수 없습니다."
+                , "가격은 공백일 수 없습니다.");
+    }
+}
---
+++ src/test/java/com/promotion/ssg_assignment1/promotion/PromotionControllerTest.java
@@ -0,0 +1,55 @@
+package com.promotion.ssg_assignment1.promotion;
+
+import com.promotion.ssg_assignment1.promotion.Dto.CreatePromotionReq;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+@WebAppConfiguration
+public class PromotionControllerTest {
+    private static ValidatorFactory validatorFactory;
+    private static Validator validatorInjected;
+
+    @BeforeAll
+    public static void init() {
+        validatorFactory = Validation.buildDefaultValidatorFactory();
+        validatorInjected = validatorFactory.getValidator();
+    }
+
+    @AfterAll
+    public static void close() {
+        validatorFactory.close();
+    }
+
+    @Test
+    @DisplayName("[프로모션 입력 테스트]")
+    void 프로모션_입력_테스트() {
+        CreatePromotionReq createPromotionReq = new CreatePromotionReq(
+                "", "2000", "",
+                LocalDate.of(2022, 01, 01),
+                LocalDate.of(2022, 02, 01)
+        );
+        Set<ConstraintViolation<CreatePromotionReq>> validate = validatorInjected.validate(createPromotionReq);
+
+        Iterator<ConstraintViolation<CreatePromotionReq>> iterator = validate.iterator();
+        List<String> messages = new ArrayList<>();
+        while (iterator.hasNext()) {
+            ConstraintViolation<CreatePromotionReq> next = iterator.next();
+            messages.add(next.getMessage());
+        }
+        Assertions.assertThat(messages).contains("프로모션 이름은 공백일 수 없습니다.");
+    }
+}
